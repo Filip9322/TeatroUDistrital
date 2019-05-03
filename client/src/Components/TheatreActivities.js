2e91425d627422ed2d0f4b2f2dc49cf51b65e2f7
Soulfilip <filip9322@gmail.com>
2019-05-03 10:48:29 -0500

    Styles on table and showing data from activies table in server

--- client/src/Components/TheatreActivities.js
+++ client/src/Components/TheatreActivities.js
@@ -1,12 +1,38 @@
 import React, {Component} from 'react'
-import PropTypes from 'prop-types'
+import './TheatreActivities.css'
 
 const Actividad = ({actividad={}}) =>{
   return(
-    <div>
-      <span>ID: {actividad.id}</span>
-      <span>{actividad.nombre}</span>
-      <p>{console.log(actividad)}</p>
+    <div className="c_actividad">
+      <div>
+        <span>ID: {actividad.id}</span>
+      </div>
+      <div>
+        <p>{actividad.nombre}</p>
+        <p><strong>Descripción:</strong> {actividad.descripcion}</p>
+      </div>
+      <div>
+        <p><strong>F. Creación:</strong> {actividad.fecha_creacion}</p>
+        <p><strong>F. Límite:</strong> {actividad.fecha_limite}</p>
+      </div>
+      <div>
+        <span>{actividad.responsable}</span>
+      </div>
+      <div>
+        <span>{actividad.sala}</span>
+      </div>
+    </div>
+  )
+}
+
+const ActivitiesHeader =() =>{
+  return(
+    <div className="h_activities">
+      <div><strong><p>ID</p></strong></div>
+      <div><strong><p>Nombre y Descripción</p></strong></div>
+      <div><strong><p>Fecha de Creación & Fecha límite</p></strong></div>
+      <div><strong><p>Responsable</p></strong></div>
+      <div><strong><p>Sala</p></strong></div>
     </div>
   )
 }
@@ -26,6 +52,7 @@ class TheatreActivities extends Component{
     return(
       <section>
         <h2>Actividades</h2>
+        <ActivitiesHeader />
         {Object.entries(activities).map((n,i) => {
             return(
               <Actividad key={i} actividad={activities[i]} />
