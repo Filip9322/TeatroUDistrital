376586c97737fbecebb94bca05946943c73fdeb6
Soulfilip <filip9322@gmail.com>
2019-05-02 21:40:57 -0500

    Adding set up files and seeds, and mainly configuration for mysql

---
+++ sql/v1_create_responsables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE  teatro_responsables (
+  id             int(11)  NOT NULL AUTO_INCREMENT,
+  nombres        TEXT     NOT NULL,
+  apellidos      TEXT     NOT NULL,
+  email          TEXT     NOT NULL,
+  cargo          TEXT     NOT NULL,
+  telefono       TEXT     NOT NULL,
+  fecha_creacion datetime NOT NULL,
+  PRIMARY KEY(id)
+)
---
+++ sql/v1_create_salas.sql
@@ -0,0 +1,7 @@
+CREATE TABLE  teatro_salas (
+  id             int(11)  NOT NULL AUTO_INCREMENT,
+  nombre         TEXT     NOT NULL,
+  horario        TEXT     NOT NULL,
+  capacidad      int(5)   NOT NULL,
+  PRIMARY KEY(id)
+)
