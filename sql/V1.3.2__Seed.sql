e929eb32a5fba51a0d0278c7221d983926d8b166
Soulfilip <filip9322@gmail.com>
2019-05-03 00:52:38 -0500

    Fake data added on seed migration

---
+++ sql/V1.3.2__Seed.sql
@@ -0,0 +1,13 @@
+INSERT INTO `mvw`.`teatro_responsables` (`nombres`, `apellidos`, `email`, `cargo`, `telefono`, `fecha_creacion`) VALUES ('Danny', 'Rincon', 'dannyrv@mail.com', 'Estudiante', '36363636', '2019-05-01 10:12:11');
+INSERT INTO `mvw`.`teatro_responsables` (`nombres`, `apellidos`, `email`, `cargo`, `telefono`, `fecha_creacion`) VALUES ('Steven', 'Calderón', 'scalderon@mail.com', 'Estudiante', '111111111', '2019-05-01 10:12:10');
+INSERT INTO `mvw`.`teatro_responsables` (`nombres`, `apellidos`, `email`, `cargo`, `telefono`, `fecha_creacion`) VALUES ('Leonardo', 'Mauricio', 'mauricio@mail.com', 'Administrativo', '123212321', '2019-05-02 06:06:06');
+
+
+INSERT INTO `mvw`.`teatro_actividades` (`nombre`, `descripcion`, `responsable`, `sala`, `estado`, `fecha_creacion`, `fecha_limite`) VALUES ('Noche de Shakespeare', '¡Celebramos el mes del teatro!', '1', '1', 'Confirmada', '2019-05-01 00:05:01', '2019-05-10 00:05:01');
+INSERT INTO `mvw`.`teatro_actividades` (`nombre`, `descripcion`, `responsable`, `sala`, `estado`, `fecha_creacion`, `fecha_limite`) VALUES ('Gil González Ávila - Montaje en Verso', '¡Celebramos el mes del teatro!', '2', '1', 'Realizada', '2019-05-01 00:05:01', '2019-05-01 00:05:01');
+INSERT INTO `mvw`.`teatro_actividades` (`nombre`, `descripcion`, `responsable`, `sala`, `estado`, `fecha_creacion`, `fecha_limite`) VALUES ('Temporada de Monólogos', '¡Celebramos el mes del teatro!', '3', '3', 'Confirmada', '2019-05-01 00:05:01', '2019-05-10 00:05:01');
+INSERT INTO `mvw`.`teatro_actividades` (`nombre`, `descripcion`, `responsable`, `sala`, `estado`, `fecha_creacion`, `fecha_limite`) VALUES ('Comedia infinita para tres actuantes y una caja', '¡Celebramos el mes del teatro!', '4', '1', 'Realizada', '2019-05-01 00:05:01', '2019-05-01 00:05:01');
+INSERT INTO `mvw`.`teatro_actividades` (`nombre`, `descripcion`, `responsable`, `sala`, `estado`, `fecha_creacion`, `fecha_limite`) VALUES ('Grupo Danza Contemporanea', 'Clases Grupo universitario de Danzas', '5', '2', 'Asignado', '2019-05-01 00:05:01', '2019-05-09 00:05:01');
+INSERT INTO `mvw`.`teatro_actividades` (`nombre`, `descripcion`, `responsable`, `sala`, `estado`, `fecha_creacion`, `fecha_limite`) VALUES ('Grupo Danza Urbana', 'Clases Grupo universitario de Danzas', '6', '2', 'Cancelado', '2019-05-01 00:05:01', '2019-05-02 00:05:01');
+INSERT INTO `mvw`.`teatro_actividades` (`nombre`, `descripcion`, `responsable`, `sala`, `estado`, `fecha_creacion`, `fecha_limite`) VALUES ('Misa', 'Tiempo Espiritual', '7', '4', 'Confirmada', '2019-05-01 00:05:01', '2019-05-10 00:05:01');
+
---
+++ sql/V1.3__Seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO `mvw`.`teatro_salas` (`nombre`, `horario`, `capacidad`) VALUES ('Principal', '8:00 am - 7:00 pm', '400');
+INSERT INTO `mvw`.`teatro_salas` (`nombre`, `horario`, `capacidad`) VALUES ('Espejos', '8:00 am - 9:00 pm', '30');
+INSERT INTO `mvw`.`teatro_salas` (`nombre`, `horario`, `capacidad`) VALUES ('Auxiliar', '8:00 am - 9:00 pm', '100');
+INSERT INTO `mvw`.`teatro_salas` (`nombre`, `horario`, `capacidad`) VALUES ('Capilla', '10:00 am - 4:00 pm', '40');
